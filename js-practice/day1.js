e7d58e7a0b51f211a9539eaa7f80e6a1497e92c1
LR-Vharun <lrvharunkumaar@gmail.com>
2025-06-12 13:20:46 +0530

    js add

---
+++ js-practice/day1.js
@@ -0,0 +1,10 @@
+console.log("Hello,,javascript!");
+
+console.log("My name is harun");
+console.log("I am"+ 25+"Years old");
+console.log(5+7);
+console.log(10*3);
+console.log("5+3="+(5+3));
+console.log("My favorite movie is Salar");
+console.log("Dream job is lead java dev");
+console.log("I have a short term memory deletiobn problem");
