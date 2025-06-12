e7e6cc02dceebf8d9cf8faf6529046b0c37a908f
LR-Vharun <lrvharunkumaar@gmail.com>
2025-06-12 19:08:50 +0530

    js is not that hard.. or is it?

---
+++ js-practice/day2.js
@@ -0,0 +1,19 @@
+let Myname="Vharun";
+let age=22;
+let occupation="Student";
+let x;
+let nothing=null;
+console.log("Value :", Myname);
+console.log("Type : ", typeof Myname);
+
+console.log("Value : ",  age);
+console.log("Type : ", typeof age);
+
+console.log("VAlue : ",occupation);
+console.log("Type : ", typeof occupation);
+
+console.log("Value : ", x);
+console.log("Type : ", typeof x);
+
+console.log("Value : ",  nothing);
+console.log("Type : ", typeof nothing);
---
+++ js-practice/day3.js
@@ -0,0 +1,19 @@
+let name="vahrun";
+let age=22;
+
+console.log("Aritmetic : ");
+console.log("Adition : ", age+5);
+console.log("Multiplication : ", age*2);
+
+console.log("\nComparision : ");
+console.log("is age equal to 22? ", age==22);
+console.log("Is age not equal to 22? :", age!=22);
+
+console.log("\nLogical");
+console.log("Is age between 18 and 30? :", 18<age && age<30);
+console.log("Is age below 18 or above 60? : ", 18>age || age>60);
+console.log("Not age >18 : ", !(age>18));
+
+console.log("\nType Check");
+console.log("Type of name : ", typeof name);
+console.log("Type of age :", typeof age);
